ed27852994d1b45e6d7b9f518c293f3a7ef887b9
SpaceCowboyZZ <131901777+SpaceCowboyZZ@users.noreply.github.com>
2024-01-27 21:56:07 -0300

    v 0.1

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 import discord
 import yt_dlp
-from datetime import datetime
 from discord.ext import commands
-# import urllib.request
-import pytube
-import os
-import re
 import asyncio
+import logging
+import sys
+from concurrent.futures import ThreadPoolExecutor
+
+
+logging.basicConfig(
+    stream=sys.stdout,
+    level=logging.DEBUG,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+)
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -15,47 +20,109 @@ intents.voice_states = True
 
 bot = commands.Bot(command_prefix='!', intents=intents)
 
-
 queue = []
 
+actual_url = []
 
-#mandar para configs.py depois
-yt_opts = {
-            'format': 'bestaudio/best',
-            'postprocessors': [{
-                'key': 'FFmpegExtractAudio',
-                'preferredcodec': 'mp3',
-                'preferredquality': '192',
-            }],
-            'default_search': 'auto',
-            'noplaylist': True,
-            'verbose': True
-}
-pl_opts = {
-            'format': 'bestaudio/best',
-            'postprocessors': [{
-                'key': 'FFmpegExtractAudio',
-                'preferredcodec': 'mp3',
-                'preferredquality': '192',
-            }],
-            'default_search': 'auto',
-            'noplaylist': False,
-            'flat_playlist': True,
-            'verbose': True
-}
+thumb_url = []
 
+embed_msg = None
 
-
-
-
-playlist_search = yt_dlp.YoutubeDL(pl_opts)
-
-yt_search = yt_dlp.YoutubeDL(yt_opts)
+thumb = None
 
 bot.play_status = False
 
-queue = []       
-        
+bot.in_chat = None
+
+executor = ThreadPoolExecutor(max_workers=5)
+
+
+async def embeded(ctx, msg, thumbna):
+    embed= discord.Embed(
+        colour=discord.Colour.brand_green(),
+        title='tocando agora:',
+        description=f'{msg}'
+            )      
+    embed.set_author(name='Tocador') 
+                
+    embed.set_image(url=thumbna)
+    
+    await ctx.send(embed=embed)
+
+
+async def search_video(ctx, urlq):  
+    yt_opts = {
+                'format': 'bestaudio/best',
+                'postprocessors': [{
+                    'key': 'FFmpegExtractAudio',
+                    'preferredcodec': 'mp3',
+                    'preferredquality': '192',
+                }],
+                'default_search': 'auto',
+                'noplaylist': True,
+                'verbose': True
+    }
+
+    yt_search = yt_dlp.YoutubeDL(yt_opts)
+    loop = asyncio.get_event_loop()
+    data = await loop.run_in_executor(executor, lambda: yt_search.extract_info(url=urlq, download=False))
+    if 'entries' in data:
+        data = data['entries'][0]       
+        data1 = [data['title'], data['url']]
+        data2 = data['webpage_url']
+        data3 = data['thumbnail']
+    else:
+        data1 = [data['title'], data['url']]
+        data2 = [data['webpage_url']]
+        data3 = [data['thumbnail']]
+    return data1, data2, data3
+
+
+async def playlist(ctx, urlq):
+    pl_opts = {
+            'format': 'bestaudio/best',
+            'postprocessors': [{
+                'key': 'FFmpegExtractAudio',
+                'preferredcodec': 'mp3',
+                'preferredquality': '192',
+            }],
+            'default_search': 'auto',     
+            'noplaylist': False,
+            'ignoreerrors': True,
+            'flat_playlist': True,
+            'verbose': True
+}
+    playlist_search = yt_dlp.YoutubeDL(pl_opts)
+    loop = asyncio.get_event_loop()
+    data = await loop.run_in_executor(executor, lambda: playlist_search.extract_info(url=urlq, download=False))
+    if 'entries' in data:
+        data1 =[[entry['title'], entry['url']] for entry in data['entries']]
+        data2 = [entry['webpage_url'] for entry in data['entries']]
+        data3 = [entry['thumbnail'] for entry in data['entries']]
+    return data1, data2, data3
+
+
+
+@bot.listen()
+async def on_voice_state_update(member, before, after):
+    if not member.id == bot.user.id:
+        return
+
+    elif before.channel is None:
+        voice = after.channel.guild.voice_client
+        time = 0
+        while True:
+            await asyncio.sleep(1)
+            time = time + 1
+            if voice.is_playing() and not voice.is_paused():
+                time = 0
+            if time == 600:
+                await voice.disconnect()
+            if not voice.is_connected():
+                break
+
+
+
 @bot.command()
 async def join(ctx):
         voice_client = discord.utils.get(bot.voice_clients, guild=ctx.guild)
@@ -63,189 +130,143 @@ async def join(ctx):
             if ctx.author.voice:
                 voice_channel = ctx.author.voice.channel
                 voice_client = await voice_channel.connect()
-            else:
-                await ctx.send('tu não ta no voice chat nengue.')
-                return
-        
+                bot.in_chat = voice_channel      
 
 @bot.command(name='play')
+@commands.cooldown(1, 2, commands.BucketType.user)
 async def play(ctx, *, search_query):
-        voice_client = discord.utils.get(bot.voice_clients, guild=ctx.guild)
-        if not voice_client:
-            await join(ctx)
-          
-          
-          
-          
-        #need to fix this     
-        if '/playlist?' in search_query:
-            data = playlist_search.extract_info(url=search_query, download=False)
-            if 'entries' in data:
-                data1 =[[entry['title'], entry['url']] for entry in data['entries']]
-     
-            data = data1.copy()
-                   
-            if data:
-                print(data)
-                
-            if bot.play_status or len(queue) > 0:
-                queue.append(data)
-                await ctx.send('adicionado a fila')
-                
-            else:
-                queue.extend(data)
-                play_song = queue[0]
-                await play_now(ctx, url=play_song[1])
-                queue.pop(0)
-                    
-            if data:
-                    print(data)
-                    
-        elif '/watch?' in search_query and not '/playlist?' in search_query:
-            data = yt_search.extract_info(url=search_query, download=False)  
-            if 'entries' in data:
-                data = data['entries'][0]
-                
-            data = [data['title'], data['url']]
-            if data:
-                print(data)
-                
-            if bot.play_status or len(queue) > 0:
-                queue.append(data)
-                await ctx.send('adicionado a fila')
-            else:
-                queue.append(data)
-                play_song = queue[0]
-                await play_now(ctx, url=play_song[1])
-                await ctx.send(f'now playing: {play_song[0]}, queue is: {queue}')
-                queue.pop(0)
+    global queue, actual_url, thumb_url
+    voice_client = discord.utils.get(bot.voice_clients, guild=ctx.guild)
+    if not voice_client:
+        await join(ctx)
+    elif ctx.author.voice.channel != bot.in_chat:
+        await ctx.send("não estamos no mesmo voice chat seu baiano.")
+        return
+    elif not voice_client:
+        await ctx.send('tu não ta no voice chat nengue.')
+        return
+    
+    if '/playlist?' in search_query and not '/watch?' in search_query:
+        await ctx.send('pegando informações da playlist, pode demorar um pouco, se vc usar "!play" novamente, a musica pode entrar na fila antes da playlist...')
+        data1, url, tn = await playlist(ctx, search_query)
+        
+        data = data1.copy()
+        actual_url.extend(url)
+        thumb_url.extend(tn)
+    
+        if bot.play_status or len(queue) > 0:
+            queue.extend(data)
+            await ctx.send(f'adicionado a fila')      
+        else:
+            queue.extend(data)
+            await play_now(ctx, url=queue.pop(0))     
+                                       
+    elif '/watch?' in search_query and not '/playlist?' in search_query:
+        data1, url, tn = await search_video(ctx, search_query)
+        
+        data = data1.copy()
+        actual_url.append(url)
+        thumb_url.append(tn)
+        
+        if bot.play_status or len(queue) > 0:
+            queue.append(data)
+            await ctx.send(f'adicionado a fila')
 
         else:
-            data = yt_search.extract_info(f'ytsearch:{search_query}', download=False) 
-            if 'entries' in data:
-                data = data['entries'][0]
-                
-            data = [data['title'], data['url']]
-
-            if bot.play_status or len(queue) > 0:
-                queue.append(data)
-                await ctx.send('adicionado a fila')
-            else:
-                queue.append(data)
-                play_song = queue.pop(0)
-                await play_now(ctx, url=play_song[1])
-                
-
-            
-               
-async def play_now(ctx, url):
-        voice_client = discord.utils.get(bot.voice_clients, guild=ctx.guild)
-        bot.is_playing = True
-        
-        ffmpeg_options = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
-
-        def after(error):
-            if error:
-                print(error)
-        
-            if len(queue) > 0 and not voice_client.is_playing():
-                next_song = queue[0]
-                voice_client.play(discord.FFmpegPCMAudio(next_song[1], **ffmpeg_options), after=lambda e: after(e))    
-                queue.pop(0)
-            else:
-                bot.is_playing = False
-                
-            
-        voice_client.play(discord.FFmpegPCMAudio(url, **ffmpeg_options), after=lambda e: after(e))
-        
-        
-        
-async def play_next(ctx):
-    if len(queue) > 0:
-        play_song = queue.pop(0)
-        await play_now(ctx, url=play_song[1])
-        
-        
-        await ctx.send(embed=discord.Embed(
-            title="Now Playing",
-            description=f"**{play_song[0]}**",
-            color=0x6b30d9
-        ).set_author(name="Tocador"))
-    
+            queue.append(data)
+            await play_now(ctx, url=queue.pop(0))
     else:
-        bot.is_playing = False
+        data1, url, tn = await search_video(ctx, search_query)
+        
+        data = data1.copy()
+        actual_url.append(url)
+        thumb_url.append(tn)
 
+        if bot.play_status or len(queue) > 0:
+            queue.append(data)
+            await ctx.send(f'adicionado a fila')
+        else:
+            queue.append(data)
+            await play_now(ctx, url=queue.pop(0))
+        
+        
+        
+async def play_now(ctx, url):
+    global embed_msg, thumb
+    voice_client = discord.utils.get(bot.voice_clients, guild=ctx.guild)
+        
+    ffmpeg_options = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
+    bot.play_status = True    
+    print(bot.play_status)
+    def after(error):
+        if error:
+            print(error)
+                
+        if bot.play_status and len(queue) > 0:
+            next_song = queue.pop(0)
+            embed_msg = f'{next_song[0]}  {actual_url.pop(0)}'
+            thumb = f'{thumb_url.pop(0)}'
+            asyncio.run_coroutine_threadsafe(embeded(ctx, embed_msg, thumb), bot.loop)
+            voice_client.play(discord.FFmpegPCMAudio(next_song[1], **ffmpeg_options), after=lambda e: after(e))
+        else:
+            bot.play_status = False
+            print(bot.play_status)
+            
+    embed_msg = f'{url[0]}  {actual_url.pop(0)}'
+    thumb = f'{thumb_url.pop(0)}'
+    await embeded(ctx, embed_msg, thumb)          
+    voice_client.play(discord.FFmpegPCMAudio(url[1], **ffmpeg_options), after=lambda e: after(e))
+        
+ 
+        
+@bot.command(name='next') #works
+async def next(ctx):
+    voice_client = discord.utils.get(bot.voice_clients, guild=ctx.guild)
+    if voice_client and voice_client.is_playing():
+        voice_client.stop()
+
+    else:
+        embed = discord.Embed(title="END", description="No song is currently playing", color=0x6b30d9)
+        embed.set_author(name="Prismo")
+        await ctx.send(embed=embed)              
                 
 
-
-
-# @bot.command(name='pause') #done
-# async def pause(ctx):
-#         global paused_at, filename
+@bot.command(name='pause') #done
+async def pause(ctx):
+    voice_client = discord.utils.get(bot.voice_clients, guild=ctx.guild)
         
-#         voice_client = discord.utils.get(bot.voice_clients, guild=ctx.guild)
-        
-#         if voice_client.is_playing():
-#             voice_client.pause()
-#             paused_at = datetime.now()
-#             await ctx.send('audio pausado')
-#             return
-#         else:
-#             await ctx.send('nenhum audio tocando no momento')
-#             return
+    if voice_client.is_playing():
+        voice_client.pause()
+        await ctx.send('audio pausado')
+        return
+    else:
+        await ctx.send('nenhum audio tocando no momento')
+        return
 
       
         
-# @bot.command(name='continue') #done
-# async def Continue(ctx):
-#         global paused_at, filename, play_task, should_stop
+@bot.command(name='continue') #done
+async def Continue(ctx):
+    voice_client = discord.utils.get(bot.voice_clients, guild=ctx.guild)
         
-#         voice_client = discord.utils.get(bot.voice_clients, guild=ctx.guild)
-        
-#         if (voice_client.is_paused() and (datetime.now() - paused_at).total_seconds() < 600):
-#                 voice_client.resume()
-#                 paused_at = datetime.now()
-#                 should_stop = True
-#                 await ctx.send('resumindo audio')
-#                 return
-                
-#         if (paused_at is not None and (datetime.now() - paused_at).total_seconds() > 600):
-#             voice_client.stop()
-#             await asyncio.sleep(0.5)
-#             if filename is not None:
-#                 try:
-#                     os.remove(filename)
-#                     await ctx.send('nenhum audio pausado no momento') 
-#                     return
-#                 except FileNotFoundError:
-#                     pass
-#                 except PermissionError as e:
-#                     await ctx.send(f'error: unable to delete file>    {e}  ')
-        
-#         if play_task is not None and not play_task.done():
-#             await ctx.send('audio já está tocando')
-
-#         else:
-#             await ctx.send('nenhum audio pausado no momento')
+    if voice_client.is_paused():
+        voice_client.resume()
+        await ctx.send('resumindo audio')
+        return
+    else:
+        await ctx.send('nenhum audio pausado no momento')
 
         
-        
-        
-# @bot.command(name='next') #works
-# async def next(ctx):
-
             
-#             voice_client = discord.utils.get(bot.voice_clients, guild=ctx.guild)
-
-
-            
-            
-            
-# @bot.command(name='stop')
-# async def stop(ctx):
-#     voice_client = discord.utils.get(bot.voice_clients, guild=ctx.guild)
-
-
+@bot.command(name='stop')
+async def stop(ctx):
+    voice_client = discord.utils.get(bot.voice_clients, guild=ctx.guild)
+    if bot.play_status or voice_client.is_paused():
+        queue.clear()
+        voice_client.stop()
+        await ctx.send('todas as musicas foram removidas da fila')
+    else:
+        await ctx.send('não estou tocando nada')
 
     # Run the bot with your token
 bot.run('MTE5Njg5NzgzMzAzMTMwMzMzOA.GKQTh_.f_l0vYlLYEvfe-sErC4L-9IhYdjubxlipusuzY')
