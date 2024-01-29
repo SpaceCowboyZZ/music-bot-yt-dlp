c41ef70fd5c39e4bd29284b9bda428690910bda6
SpaceCowboyZZ <131901777+SpaceCowboyZZ@users.noreply.github.com>
2024-01-29 18:01:42 -0300

    still need a way to make a custom help

--- main.py
+++ main.py
@@ -20,6 +20,7 @@ intents.voice_states = True
 
 bot = commands.Bot(command_prefix='!', intents=intents)
 
+#globals because i'm lazy
 queue = []
 
 actual_url = []
@@ -102,6 +103,24 @@ async def playlist(ctx, urlq):
     return data1, data2, data3
 
 
+async def join(ctx):
+    voice_client = discord.utils.get(bot.voice_clients, guild=ctx.guild)
+    if not voice_client:
+        if ctx.author.voice:
+            voice_channel = ctx.author.voice.channel
+            voice_client = await voice_channel.connect()
+            bot.in_chat = voice_channel  
+            
+        elif not voice_client:
+            await ctx.send('tu não ta no voice chat nengue.')
+        
+    elif ctx.author.voice.channel != bot.in_chat:
+        await ctx.send("não estamos no mesmo voice chat seu baiano.")
+        return
+    
+    return voice_client
+
+
 
 @bot.listen()
 async def on_voice_state_update(member, before, after):
@@ -123,27 +142,12 @@ async def on_voice_state_update(member, before, after):
 
 
 
-@bot.command()
-async def join(ctx):
-        voice_client = discord.utils.get(bot.voice_clients, guild=ctx.guild)
-        if not voice_client:
-            if ctx.author.voice:
-                voice_channel = ctx.author.voice.channel
-                voice_client = await voice_channel.connect()
-                bot.in_chat = voice_channel      
-
 @bot.command(name='play')
 @commands.cooldown(1, 2, commands.BucketType.user)
 async def play(ctx, *, search_query):
     global queue, actual_url, thumb_url
-    voice_client = discord.utils.get(bot.voice_clients, guild=ctx.guild)
+    voice_client = await join(ctx)
     if not voice_client:
-        await join(ctx)
-    elif ctx.author.voice.channel != bot.in_chat:
-        await ctx.send("não estamos no mesmo voice chat seu baiano.")
-        return
-    elif not voice_client:
-        await ctx.send('tu não ta no voice chat nengue.')
         return
     
     if '/playlist?' in search_query and not '/watch?' in search_query:
@@ -156,7 +160,8 @@ async def play(ctx, *, search_query):
     
         if bot.play_status or len(queue) > 0:
             queue.extend(data)
-            await ctx.send(f'adicionado a fila')      
+            embed = discord.Embed(title='Adicionado a fila', colour= discord.Colour.blue())
+            await ctx.send(embed=embed)      
         else:
             queue.extend(data)
             await play_now(ctx, url=queue.pop(0))     
@@ -170,8 +175,8 @@ async def play(ctx, *, search_query):
         
         if bot.play_status or len(queue) > 0:
             queue.append(data)
-            await ctx.send(f'adicionado a fila')
-
+            embed = discord.Embed(title='Adicionado a fila', colour= discord.Colour.blue())
+            await ctx.send(embed=embed)
         else:
             queue.append(data)
             await play_now(ctx, url=queue.pop(0))
@@ -184,7 +189,8 @@ async def play(ctx, *, search_query):
 
         if bot.play_status or len(queue) > 0:
             queue.append(data)
-            await ctx.send(f'adicionado a fila')
+            embed = discord.Embed(title='Adicionado a fila', colour= discord.Colour.blue())
+            await ctx.send(embed=embed)
         else:
             queue.append(data)
             await play_now(ctx, url=queue.pop(0))
@@ -193,11 +199,12 @@ async def play(ctx, *, search_query):
         
 async def play_now(ctx, url):
     global embed_msg, thumb
-    voice_client = discord.utils.get(bot.voice_clients, guild=ctx.guild)
+    voice_client = await join(ctx)
+    if not voice_client:
+        return
         
     ffmpeg_options = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
     bot.play_status = True    
-    print(bot.play_status)
     def after(error):
         if error:
             print(error)
@@ -210,7 +217,6 @@ async def play_now(ctx, url):
             voice_client.play(discord.FFmpegPCMAudio(next_song[1], **ffmpeg_options), after=lambda e: after(e))
         else:
             bot.play_status = False
-            print(bot.play_status)
             
     embed_msg = f'{url[0]}  {actual_url.pop(0)}'
     thumb = f'{thumb_url.pop(0)}'
@@ -219,54 +225,89 @@ async def play_now(ctx, url):
         
  
         
-@bot.command(name='next') #works
+@bot.command(name='next')
+@commands.cooldown(1, 2, commands.BucketType.user)
 async def next(ctx):
-    voice_client = discord.utils.get(bot.voice_clients, guild=ctx.guild)
-    if voice_client and voice_client.is_playing():
+    voice_client = await join(ctx)
+    if not voice_client:
+        return
+    
+    if voice_client and (voice_client.is_playing() or voice_client.is_paused()):
         voice_client.stop()
-
     else:
-        embed = discord.Embed(title="END", description="No song is currently playing", color=0x6b30d9)
-        embed.set_author(name="Prismo")
-        await ctx.send(embed=embed)              
+        embed = discord.Embed(title="Erro", description="Nada tocando no momento", color=discord.Colour.brand_red())
+        await ctx.send(embed=embed)          
+    
                 
 
-@bot.command(name='pause') #done
+@bot.command(name='pause')
+@commands.cooldown(1, 2, commands.BucketType.user)
 async def pause(ctx):
-    voice_client = discord.utils.get(bot.voice_clients, guild=ctx.guild)
+    voice_client = await join(ctx)
+    if not voice_client:
+        return
         
     if voice_client.is_playing():
         voice_client.pause()
-        await ctx.send('audio pausado')
+        embed = discord.Embed(title='Pausado', color=discord.Colour.blue())
+        await ctx.send(embed=embed)
         return
     else:
-        await ctx.send('nenhum audio tocando no momento')
+        embed = discord.Embed(title="Erro", description="Nada tocando no momento", color=discord.Colour.brand_red())
+        await ctx.send(embed=embed) 
         return
 
       
         
-@bot.command(name='continue') #done
+@bot.command(name='continue')
+@commands.cooldown(1, 2, commands.BucketType.user)
 async def Continue(ctx):
-    voice_client = discord.utils.get(bot.voice_clients, guild=ctx.guild)
+    voice_client = await join(ctx)
+    if not voice_client:
+        return
         
     if voice_client.is_paused():
         voice_client.resume()
-        await ctx.send('resumindo audio')
+        embed = discord.Embed(title='Continuando', colour= discord.Colour.brand_green())
+        await ctx.send(embed=embed)
         return
     else:
-        await ctx.send('nenhum audio pausado no momento')
+        embed = discord.Embed(title='Erro', description='Nada pausado no momento', colour=discord.Colour.brand_red())
+        await ctx.send(embed=embed)
 
         
-            
+           
 @bot.command(name='stop')
+@commands.cooldown(1, 2, commands.BucketType.user)
 async def stop(ctx):
-    voice_client = discord.utils.get(bot.voice_clients, guild=ctx.guild)
+    voice_client = await join(ctx)
+    if not voice_client:
+        return
+    
     if bot.play_status or voice_client.is_paused():
         queue.clear()
         voice_client.stop()
-        await ctx.send('todas as musicas foram removidas da fila')
+        embed = discord.Embed(title='Parando', description='Todas as musicas foram removidas da fila', colour=discord.Colour.brand_red())
+        await ctx.send(embed=embed)
     else:
-        await ctx.send('não estou tocando nada')
+        embed = discord.Embed(title='Erro', description='Não estou tocando nada', colour=discord.Colour.brand_red())
+        await ctx.send(embed=embed)
+        
 
-    # Run the bot with your token
+
+class MyHelp(commands.HelpCommand):
+    async def send_bot_help(self, mapping):
+        embed = discord.Embed(title='Help', colour= discord.Colour.gold())
+        for cog, commands in mapping.items():
+           command_signatures = [self.get_command_signature(c) for c in commands]
+           if command_signatures:
+                cog_name = getattr(cog, "qualified_name", "Comandos")
+                embed.add_field(name=cog_name, value="\n".join(command_signatures), inline=False)
+
+        channel = self.get_destination()
+        await channel.send(embed=embed)
+
+bot.help_command = MyHelp()
+
+# Run the bot with your token
 bot.run('MTE5Njg5NzgzMzAzMTMwMzMzOA.GKQTh_.f_l0vYlLYEvfe-sErC4L-9IhYdjubxlipusuzY')
